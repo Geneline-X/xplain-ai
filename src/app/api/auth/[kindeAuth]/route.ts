84b37ab9ea30615b1762d1869605028c505b5b70
Dennis2340 <kamaradennis36@gmail.com>
2024-01-05 16:46:56 +0000

    typying the handleAuth

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,7 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest, {params}: any): Promise<void | Response | any> {
+export async function GET(request: NextRequest, {params}: any): Promise<void | Response> {
 	const endpoint:string = params.kindeAuth;
 	return handleAuth(request, endpoint);
 }
