44c775006afa28c378fe91d624706d9bbc05dc70
Dennis2340 <kamaradennis36@gmail.com>
2024-01-05 17:16:49 +0000

    testing if it works

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -2,6 +2,7 @@ import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest, {params}: any): Promise<void | Response> {
-	const endpoint = params.kindeAuth;
+	const endpoint:string = params.kindeAuth;
 	return handleAuth(request, endpoint) as Promise<void | Response>;
+	
 }
